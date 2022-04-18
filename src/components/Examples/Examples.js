d78bdaa0db7e9a1d097c02fabc12301f9920aa64
Saoa35 <saoamah@gmail.com>
2022-04-18 22:02:34 +0200

    + 3 div

--- src/components/Examples/Examples.js
+++ src/components/Examples/Examples.js
@@ -3,12 +3,16 @@ import React from "react";
 class Examples extends React.Component {
 
     render() {
-
+        const str = 'some text';
         return (
             <div>
                 <h2>
                     Examples
                 </h2>
+                <div>{str}</div>
+                <div>{str}</div>
+                <div>{str}</div>
+
             </div>
         )
     }
