730fcd90b9df04e0df83b58033b27b116d115a77
Saoa35 <saoamah@gmail.com>
2022-04-18 21:18:29 +0200

    example js

---
+++ src/components/Examples/Examples.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+class Examples extends React.Component {
+
+    render() {
+
+        return (
+            <div>
+                <h2>
+                    Examples
+                </h2>
+            </div>
+        )
+    }
+}
+
+export default Examples;
